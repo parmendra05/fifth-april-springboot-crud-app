b3c13bfb889451feaa0ba02ebf77f36d9bf362e4
parmendra kumar <parmendrakumar718@gmail.com>
2022-04-11 08:20:20 +0530

    added logic for BookNotFound

--- src/main/java/com/pack/service/BookServiceImpl.java
+++ src/main/java/com/pack/service/BookServiceImpl.java
@@ -1,9 +1,12 @@
 package com.pack.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pack.entity.Book;
+import com.pack.error.BookNotFoundException;
 import com.pack.repository.BookDao;
 
 @Service
@@ -17,8 +20,11 @@ public class BookServiceImpl implements BookService {
 	}
 
 	public Book getBook(Integer id) {
-		Book theBook = dao.findById(id).get();
-		return theBook;
+		Optional<Book> theBook = dao.findById(id);
+		if (theBook.isPresent())
+			return theBook.get();
+		else
+			throw new BookNotFoundException("Book doesn't exit for id : "+id);
 	}
 
 	@Override
